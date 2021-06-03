b73bbcc0c6d38a2c781529586830797117e28d5b
Zenng3 <85269748+Zenng3@users.noreply.github.com>
2021-06-03 13:21:34 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'mày à!',
+    introDesc: `dù qua 1/6 hay còn gọi là
+    quốc tế thiếu nhi á ,mày có muốn tặng 
+    quà không hãy trả lời cho tao biết`,
+    btnIntro: 'ok',
+    title: 'Phải chăng mày không muốn tặng quà 😙',
+    desc: 'Phải chăng mày đã trưởng thành ',
+    btnYes: 'đúng rồi tao đã trưởng thành',
+    btnNo: 'không t vẫn muốn tặng quà',
+    question:'Trên thế giới hơn 7 tỉ người mà mày lại đòi quà t',
+    btnReply: 'gửi cho tao',
+    reply: 'thôi t không cần quà nữa đâu',
+    mess: 'tao biết mà cảm ơn mày nhiều',
+    messDesc: 'vày là m không cần quà đúng không',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.messenger.com/t/100057340044692'
+}
